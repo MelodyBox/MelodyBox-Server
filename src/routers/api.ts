ba31670966534c045498e9478cb3d3f28fb623e4
MrPancakes39 <selenahasan39@proton.me>
2023-03-24 20:06:57 +0200

    Renamed controller

--- src/routers/api.ts
+++ src/routers/api.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import * as v from "../controller/requestController";
+import * as v from "../controller/validationController";
 const router = Router();
 
 router.get("/search", v.validSearchRequest);
