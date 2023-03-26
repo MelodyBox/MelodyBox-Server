7f582c0b2d7c3ddceda9b8d8e790d1be8b0d9d48
MrPancakes39 <selenahasan39@proton.me>
2023-03-26 12:03:28 +0300

    Added Search Test

--- tests/ytm.test.ts
+++ tests/ytm.test.ts
@@ -20,4 +20,51 @@ describe("Youtube Music Client Tests", () => {
     expect(results.length).toBeGreaterThanOrEqual(15);
   });
 
+  test("Search", async () => {
+    expect.assertions(14);
+    let results;
+
+    const query = "edm playlist";
+    await expect(ytm.search({ query, filter: "song" })).rejects.toMatch("error");
+    await expect(ytm.search({ query, scope: "upload" })).rejects.toMatch("error");
+
+    results = await ytm.search("l1qwkfkah2l1qwkfkah2");
+    expect(results.length).toBeLessThanOrEqual(2);
+
+    const queries = ["taylor swift", "taylor swift blank space", "taylor swift fearless"];
+    queries.forEach(async (q) => {
+      const results = await ytm.search({ query: q });
+      expect(results.length).toBeGreaterThan(10);
+    });
+
+    results = await ytm.search({
+      query: "Martin Stig Andersen - Deteriation",
+      ignore_spelling: true,
+    });
+    expect(results.length).toBeGreaterThan(0);
+
+    results = await ytm.search({ query, filter: "songs" });
+    expect(results.length).toBeGreaterThan(10);
+
+    results = await ytm.search({ query, filter: "videos" });
+    expect(results.length).toBeGreaterThan(10);
+
+    results = await ytm.search({ query, filter: "albums", limit: 40 });
+    expect(results.length).toBeGreaterThan(20);
+
+    results = await ytm.search({ query: "project-2", filter: "artists", ignore_spelling: true });
+    expect(results.length).toBeGreaterThan(0);
+
+    results = await ytm.search({ query: "classical music", filter: "playlists" });
+    expect(results.length).toBeGreaterThan(5);
+
+    results = await ytm.search({ query: "clasical music", filter: "playlists", ignore_spelling: true });
+    expect(results.length).toBeGreaterThan(5);
+
+    results = await ytm.search({ query: "clasic rock", filter: "community_playlists", ignore_spelling: true });
+    expect(results.length).toBeGreaterThan(5);
+
+    results = await ytm.search({ query: "hip hop", filter: "featured_playlists" });
+    expect(results.length).toBeGreaterThan(5);
+  });
 });
