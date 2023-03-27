d2e76271c02a1978e6634b6ee51cc2123b282d26
MrPancakes39 <selenahasan39@proton.me>
2023-03-27 21:40:19 +0300

    Re-Returned to Express Server

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,10 @@
-// import express from "express";
-// const app = express();
-// const PORT = process.env.PORT || 5173;
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 5173;
 
-// import apiRouter from "./routers/api";
-// app.use("/", apiRouter);
+import apiRouter from "./routers/api";
+app.use("/", apiRouter);
 
-// app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}/`));
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}/`));
 
-// export const expressApp = app;
-
-import YTMusicClient from "./utils/YTMusicClient";
-const ytm = new YTMusicClient();
-
-async function main() {
-  const result = await ytm.search({ query: "Born this way" });
-  console.log(result[0]);
-}
-
-main();
+export const expressApp = app;
