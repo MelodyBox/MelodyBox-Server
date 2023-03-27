4e635adfe71df7fcfaa2a81e01be045158de63ac
MrPancakes39 <selenahasan39@proton.me>
2023-03-27 20:42:06 +0300

    Returned to Express Server

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,10 @@
-// import express from "express";
-// const app = express();
-// const PORT = process.env.PORT || 5173;
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 5173;
 
-// import apiRouter from "./routers/api";
-// app.use("/", apiRouter);
+import apiRouter from "./routers/api";
+app.use("/", apiRouter);
 
-// app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}/`));
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}/`));
 
-// export const expressApp = app;
-
-import YTMusicClient from "./utils/YTMusicClient";
-const ytm = new YTMusicClient();
-
-async function main() {
-  try {
-    const res = await ytm.getArtist("UCaPIRYCKs51kvD4jrbwMH1w");
-    console.log("res", res.name);
-  } catch {
-    console.log("err");
-  }
-}
-
-main();
+export const expressApp = app;
