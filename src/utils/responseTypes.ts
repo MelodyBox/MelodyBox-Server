7119fdf8c36f0415d1a0d9b202c8dafb5c242e04
MrPancakes39 <selenahasan39@proton.me>
2023-03-30 17:14:04 +0300

    Added ApiResponse type

--- src/utils/responseTypes.ts
+++ src/utils/responseTypes.ts
@@ -10,16 +10,28 @@ type ErrorResOptions = {
   message: string;
 };
 
+export type SuccessRes<T> = {
+  success: true;
+  data: T;
+};
+
+export type ErrorRes = {
+  success: false;
+  error: string;
+};
+
+export type ApiResponse<T> = SuccessRes<T> | ErrorRes;
+
 export function SuccessRes<T>(res: Response, options: SuccessResOptions<T>) {
   return res.status(options.code ?? 200).json({
     success: true,
     data: options.data,
-  });
+  } as SuccessRes<T>);
 }
 
 export function ErrorRes(res: Response, options: ErrorResOptions) {
   return res.status(options.code ?? 400).json({
     success: false,
     error: options.message,
-  });
+  } as ErrorRes);
 }
