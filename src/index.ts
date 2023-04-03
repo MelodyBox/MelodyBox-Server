dabc2d2c46b97a9e5f716e54d2ed901b7f461781
MrPancakes39 <selenahasan39@proton.me>
2023-04-03 18:19:57 +0300

    Added helmet for security

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import "./env";
 import express from "express";
+import helmet from "helmet";
 const app = express();
 const PORT = process.env.PORT || 5173;
 
 import apiRouter from "./routers/api";
 app.use("/", apiRouter);
+app.use(helmet());
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}/`));
 
