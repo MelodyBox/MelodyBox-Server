e48b38b225526a156977bae7cb1f91afdfd1cc20
MrPancakes39 <selenahasan39@proton.me>
2023-04-15 16:35:04 +0300

    Enabled cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import "./env";
 import express from "express";
 import helmet from "helmet";
+import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 6012;
 
 import apiRouter from "./routers/api";
-app.use("/", apiRouter);
 app.use(helmet());
+app.use(cors());
+app.use("/", apiRouter);
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}/`));
 
