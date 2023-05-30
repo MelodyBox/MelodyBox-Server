23caf041717ea361baf8c7648f390da9ad4e6917
MrPancakes39 <salmannhassan39@gmail.com>
2023-05-30 09:58:26 +0300

    Removed Python Dependency

--- src/utils/YTMusicClient/YTMusic.ts
+++ src/utils/YTMusicClient/YTMusic.ts
@@ -1,25 +1,3 @@
-import { interpreter as py, PyModule, PyObject } from "node-calls-python";
-import path from "path";
-import fs from "fs";
-
 export class _YTMusic {
-  #pyMod: PyModule;
-  #pyYT: PyObject;
-
-  constructor() {
-    // ensures the python file always exist for build
-    const pyFile = path.resolve(__dirname, "ytmusicconnect.py");
-    fs.writeFileSync(pyFile, "from ytmusicapi import YTMusic", { encoding: "utf-8" });
-    this.#pyMod = py.importSync(pyFile);
-    this.#pyYT = py.createSync(this.#pyMod, "YTMusic");
-  }
-
-  protected pythonCall(functionName: string, ...args: unknown[]) {
-    try {
-      const result = py.callSync(this.#pyYT, functionName, ...args);
-      return { success: true, data: result };
-    } catch (e) {
-      return { success: false, error: (e as Error).message };
-    }
-  }
+  constructor() {}
 }
